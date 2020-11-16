7f598b86701f0b292f0f2112175a4a8cb3d6c5b0
Yalchin403 <54992849+Yalchin403@users.noreply.github.com>
2020-11-17 00:07:25 +0400

    some exercises added

--- loops.cpp
+++ loops.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
 using namespace std;
-int main() {
+// int main() {
 
-    int num;
-    cout << "Please enter the number: ";
-    cin >> num;
+//     int num;
+//     cout << "Please enter the number: ";
+//     cin >> num;
 
-    int bolen = 1;
-    int max = num + 1;
-    while( bolen != max){
-        if (num % bolen == 0){
-            cout << bolen << " ";
+//     int bolen = 1;
+//     int max = num + 1;
+//     while( bolen != max){
+//         if (num % bolen == 0){
+//             cout << bolen << " ";
+//         }
+//         bolen++;
+//     }
+
+
+// }
+
+
+
+// int main(){
+
+//     int num;
+//     cout << "Enter the number: ";
+//     cin >> num;
+
+//     string checker = "sadedir";
+//     for (int i = 2; i < num; i++){
+//         if (num % i == 0 && num != 1 && num != 3){
+//             checker = "sade deyil";
+//             break;
+//         }
+//     }
+//     cout << checker;
+
+// }
+
+
+int main(){
+
+    for(int i = 1; i < 100; i++){
+        int count = 0;
+        for(int k = 1; k <= i; k++){
+            if(i % k == 0){
+                    count++;
+            }
         }
-        bolen++;
-    }
 
+        if(count <= 2){
+            cout << i << " ";
+        }
+    }
 
 }
