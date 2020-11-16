b2446ff8a737bbda7ecf281cd88bd9225f3ae2c1
Yalchin403 <54992849+Yalchin403@users.noreply.github.com>
2020-11-17 01:06:56 +0400

    Solutions including exercise 14 (fibonacci)

--- loops.cpp
+++ loops.cpp
@@ -38,19 +38,80 @@ using namespace std;
 // }
 
 
-int main(){
+// int main(){
 
-    for(int i = 1; i < 100; i++){
-        int count = 0;
-        for(int k = 1; k <= i; k++){
-            if(i % k == 0){
-                    count++;
-            }
-        }
+//     for(int i = 1; i < 100; i++){
+//         int count = 0;
+//         for(int k = 1; k <= i; k++){
+//             if(i % k == 0){
+//                     count++;
+//             }
+//         }
 
-        if(count <= 2){
-            cout << i << " ";
-        }
-    }
+//         if(count <= 2){
+//             cout << i << " ";
+//         }
+//     }
 
-}
+// }
+
+
+
+// int main(){
+//     int num1, num2, num3;
+//     cout << "Please enter three numbers: ";
+//     cin >> num1 >> num2 >> num3;
+
+//     for (int i = num1; i < num2; i++){
+
+//         int result = i * num3;
+//         cout << result << " ";
+
+//     }
+
+// }
+
+
+// int main(){
+
+//     int sum = 0;
+    
+//     for (int i = 1; i < 101; i++){
+
+//         sum += i;
+//     }
+//     double average = sum/100;
+//     cout << "sum is " << sum << " and average is " << average; 
+// }
+
+// int main(){
+
+//     double sum = 0;
+//     for (int i = 1; i < 50; i++){
+
+//         sum += 1.0/i;
+//     }
+
+//     cout << sum;
+// }
+
+// int main(){
+
+//     int a, b, c;
+//     a = 0;
+//     b = 1;
+//     int n;  //n sequence
+//     cout << "Please enter the number of sequence: ";
+//     cin >> n;
+//     for( int i = 0; i < n; i++){
+//         if (i == 0){
+//             cout << 0 << " " << 1 << " ";
+//         }
+//         c = a + b;
+//         a = b;
+//         b = c;
+
+//         cout << c << " ";
+
+//     }
+// }
