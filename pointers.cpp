ecc46d307fc0f7aff6904770e6e3470bdcfb9f97
Yalchin403 <54992849+Yalchin403@users.noreply.github.com>
2020-12-03 03:13:16 +0400

    pointers started

---
+++ pointers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+// int main (){
+//     int x;
+//     cout << "x -in unvani = " << &x ;
+
+// }
+
+// int main(){
+//  int x, *y;
+//  //x -e her hansi qiymet verek
+//  x = 12;
+//  //y-e x-in unvanin menimsedek
+//  y = &x;
+//  //x-in unvanin cap edek
+//  cout << "x -in unvani = " << y << "\n";
+//  //x -de olan melumati cap edek
+//  cout << "x -in qiymeti = " << *y << "\n";
+// }
