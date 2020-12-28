9f96cb90ef7a2b4dd1324c38e4fc6c5453c2f15d
Yalchin403 <54992849+Yalchin403@users.noreply.github.com>
2020-12-28 11:13:35 +0400

    functions.cpp edited

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// int my_function(int x, int y){
+//     int z;
+//     z = x + y;
+//     cout << z;
+//     return 0;
+// }
+
+
+// void helloWorld(){
+//     cout << "Hello World, I'm alive!";
+// }
+
+// int sg( int x){
+//     x ++;
+//     return x;
+// }
+
+// int main(){
+//     int y;
+//     y = sg(5);
+//     cout << "y is " << y;
+// }
+
+int main() {
+	cout << "Hello World";
+}
